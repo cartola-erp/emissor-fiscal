f6abd261af9b170616389fea854711b5b91c72eb
Murilo de Moraes Tuvani <murilo@ag.local>
2019-10-19 10:59:16 -0300

    Estrutura basica do projeto

---
+++ src/test/java/net/cartola/emissorfiscal/emissorfiscal/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cartola.emissorfiscal.emissorfiscal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
