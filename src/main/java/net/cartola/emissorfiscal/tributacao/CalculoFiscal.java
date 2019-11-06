7b5888109648b90f09fa11dce9c0016614ec39d6
Robson Henrique <robsonhenrique16@hotmail.com>
2019-11-06 14:46:48 -0200

    [update] Retorno para CalculoFiscal e não um Map

--- src/main/java/net/cartola/emissorfiscal/tributacao/CalculoFiscal.java
+++ src/main/java/net/cartola/emissorfiscal/tributacao/CalculoFiscal.java
@@ -1,10 +1,8 @@
 package net.cartola.emissorfiscal.tributacao;
 
-import java.util.Map;
-
 import net.cartola.emissorfiscal.documento.DocumentoFiscal;
 
 public interface CalculoFiscal {
 	
-	Map<String, String> calculaImposto(DocumentoFiscal documentoFiscal);
+	CalculoImposto calculaImposto(DocumentoFiscal documentoFiscal);
 }
