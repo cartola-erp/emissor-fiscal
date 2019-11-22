31105063bce13138f03d041e76b1ae0c4249f64b
Robson Henrique <robsonhenrique16@hotmail.com>
2019-11-22 17:39:05 -0300

    [add] Class Pessoa

--- src/main/java/net/cartola/emissorfiscal/documento/Pessoa.java
+++ src/main/java/net/cartola/emissorfiscal/documento/Pessoa.java
@@ -1,5 +1,89 @@
 package net.cartola.emissorfiscal.documento;
 
-public enum Pessoa {
-	FISICA, JURIDICA
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import net.cartola.emissorfiscal.estado.EstadoSigla;
+
+/**
+ *	22 de nov de 2019
+ *	@author robson.costa
+ */
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+	
+	private Long id;
+	private Long cnpj;
+	private String regimeApuracao;
+	private EstadoSigla uf = EstadoSigla.SP;
+	private PessoaTipo pessoaTipo = PessoaTipo.FISICA;
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(Long cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	//	@NotNull(message = ESTADO_EMITENTE_OBRIGATORIO)
+	@Enumerated(EnumType.STRING)
+	public EstadoSigla getEmitenteUf() {
+		return uf;
+	}
+
+	public void setEmitenteUf(EstadoSigla emitenteUf) {
+		this.uf = emitenteUf;
+	}
+
+	public String getEmitenteRegimeApuracao() {
+		return regimeApuracao;
+	}
+
+	public void setEmitenteRegimeApuracao(String regimeApuracao) {
+		this.regimeApuracao = regimeApuracao;
+	}
+
+	public String getRegimeApuracao() {
+		return regimeApuracao;
+	}
+
+	public void setRegimeApuracao(String regimeApuracao) {
+		this.regimeApuracao = regimeApuracao;
+	}
+
+	public EstadoSigla getUf() {
+		return uf;
+	}
+
+	public void setUf(EstadoSigla uf) {
+		this.uf = uf;
+	}
+
+	public PessoaTipo getPessoaTipo() {
+		return pessoaTipo;
+	}
+
+	public void setPessoaTipo(PessoaTipo pessoaTipo) {
+		this.pessoaTipo = pessoaTipo;
+	}
+	
 }
+
+
---
+++ src/main/java/net/cartola/emissorfiscal/documento/PessoaTipo.java
@@ -0,0 +1,5 @@
+package net.cartola.emissorfiscal.documento;
+
+public enum PessoaTipo {
+	FISICA, JURIDICA
+}
