b9d04ef9cced882953f44dec4549fc610f33f696
Robson Henrique <robsonhenrique16@hotmail.com>
2019-11-23 08:42:10 -0300

    [add] Tela de tributação Estadual. PS: Ainda está inacabada

---
+++ src/main/java/net/cartola/emissorfiscal/tributacao/estadual/TributacaoEstadualController.java
@@ -0,0 +1,105 @@
+package net.cartola.emissorfiscal.tributacao.estadual;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import net.cartola.emissorfiscal.estado.EstadoService;
+import net.cartola.emissorfiscal.ncm.NcmService;
+import net.cartola.emissorfiscal.operacao.OperacaoService;
+
+@RequestMapping("/tributacaoEstadual")
+@RestController
+public class TributacaoEstadualController {
+	
+	@Autowired
+	private TributacaoEstadualService icmsService;
+	
+	@Autowired
+	private EstadoService estadoService;
+	
+	@Autowired
+	private OperacaoService operacaoService;
+	
+	@Autowired
+	private NcmService ncmService;
+	
+	@GetMapping("/cadastro")
+	public ModelAndView loadTributacaoEstadual() {
+		ModelAndView mv = new ModelAndView("tributacao-estadual/cadastro");
+		TributacaoEstadual icms = new TributacaoEstadual();
+
+		mv.addObject("icms ", icms);
+//		mv.addObject("listEstado", estadoService.findAll());
+		mv.addObject("listEstado", estadoService.findAll());
+//		mv.addObject("estadoOrigem", new Estado());
+		mv.addObject("listOperacao", operacaoService.findAll());
+		mv.addObject("listNcms", ncmService.findAll());
+		
+		// mv.addObject("textBtnCadastrarEditar", "Cadastrar");
+		return mv;
+	}
+	
+	@PostMapping("/cadastro")
+	public ModelAndView save(@Valid TributacaoEstadual icms, @RequestParam("ufOrigem") String ufOrigem, @RequestParam("ufDestino") String ufDestino, BindingResult result, RedirectAttributes attributes) {
+	/*	"Receber" a ufOrigem e Destino do form, passar essa "lista" de sigla, para consultar os estados,
+	 * setando assim dentro do obj icms, para depois salvar
+	 * 
+	 */
+//		Set<Estado> estados = estadoService.findEstadoBySiglasIn( estados);
+		
+		if (result.hasErrors() ){
+			ModelAndView mv = new ModelAndView("tributacao-estadual/cadastro");
+			mv.addObject("icms ", icms);
+//			mv.addObject("mensagemErro", icmsService.getMensagensErros(result, existeNumeroEExecao));
+			return mv;
+		}
+		ModelAndView mv = new ModelAndView("redirect:/tributacaoEstadual/cadastro");
+	
+		icmsService.save(icms);
+		attributes.addFlashAttribute("mensagemSucesso", "ICMS alterado/cadastrado com sucesso!");
+		return mv;
+	}
+		
+	@GetMapping("/consulta")
+	public ModelAndView findAll() {
+		ModelAndView mv = new ModelAndView("tributacaoEstadual/consulta");
+		mv.addObject("listNcm", icmsService.findAll());
+		
+		return mv;
+	}
+
+//	@PostMapping("/consulta")
+//	public ModelAndView findByNumero(@RequestParam("numeroNcm") String numeroNcm, Model model) {
+//		ModelAndView mv = new ModelAndView("tributacaoEstadual/consulta");
+//		mv.addObject("listNcm", icmsService.findByNumero(Integer.parseInt(numeroNcm)));
+//
+//		return mv;
+//	}
+//
+//	// Método que irá carregar na tela de cadastro, os valores cadastrados de um NCM (para poder editar)
+//	@GetMapping("/editar/{id}")
+//	public ModelAndView edit(@PathVariable long id) {
+//		ModelAndView mv = new ModelAndView("tributacaoEstadual/cadastro");
+//		Tributação Estadual tributacaoEstadual = icmsService.findOne(id).get();
+//		mv.addObject("tributacaoEstadual", tributacaoEstadual);
+//		// mv.addObject("textBtnCadastrarEditar", "Editar");
+//
+//		return mv;
+//	}
+//
+//	@PostMapping("/deletar/{id}")
+//	public ModelAndView delete(@PathVariable("id") long id, RedirectAttributes attributes) {
+//		icmsService.deleteById(id);
+//		attributes.addFlashAttribute("mensagemSucesso", "Tributação Estadual deletado com sucesso!");
+//		return new ModelAndView("redirect:/tributacaoEstadual/consulta");
+//	}
+}
