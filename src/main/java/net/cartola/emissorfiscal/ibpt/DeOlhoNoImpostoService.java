487a0dda9e50ee4c4229c404860fefedc4f7c1ee
Robson Henrique <robsonhenrique16@hotmail.com>
2020-03-18 14:27:27 -0300

    [update] calc De olho no imposto

--- src/main/java/net/cartola/emissorfiscal/ibpt/DeOlhoNoImpostoService.java
+++ src/main/java/net/cartola/emissorfiscal/ibpt/DeOlhoNoImpostoService.java
@@ -1,9 +1,13 @@
 package net.cartola.emissorfiscal.ibpt;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import net.cartola.emissorfiscal.documento.DocumentoFiscal;
+import net.cartola.emissorfiscal.documento.DocumentoFiscalItem;
 
 
 /**
@@ -24,7 +28,60 @@ public class DeOlhoNoImpostoService {
 		return olhoNoImpostoRepository.findById(id);
 	}
 	
-	//  fazer calculo
+	//  ========================================================= CALCULO =================================================================
+	
+	public BigDecimal getTotalImposto(Optional<DocumentoFiscal> opDocFiscal) {
+		BigDecimal totalImposto = BigDecimal.ZERO;
+		if (opDocFiscal.isPresent()) {
+			setDeOlhoNoImposto(opDocFiscal);
+			DocumentoFiscal docFiscal = opDocFiscal.get();
+			totalImposto.add(docFiscal.getValorImpostoFederal()
+					.add(docFiscal.getValorImpostoEstadual()
+					.add(docFiscal.getValorImpostoMunicipal())));
+		}
+		return totalImposto;
+	}
+	
+	// ACho melhor só listar os impostos aqui retornar numa lista,
+	// daí em outro método eu totalizo
+	public void setDeOlhoNoImposto(Optional<DocumentoFiscal> opDocFiscal) {
+		DeOlhoNoImposto[] opOlhoNoImposto = new DeOlhoNoImposto[0];
+		BigDecimal vlrImpostoFederal = BigDecimal.ZERO;
+		BigDecimal vlrImpostoEstadual = BigDecimal.ZERO;
+		BigDecimal vlrImpostoMunicipal = BigDecimal.ZERO;
+		
+		if (opDocFiscal.isPresent()) {
+			opDocFiscal.get().getItens().stream().forEach(item -> {
+				opOlhoNoImposto[0] = findNcmByNumeroAndExcecao(item.getNcm().getNumero(), Integer.toString(item.getNcm().getExcecao())).get();
+				BigDecimal totalItem = totalItem(item);
+				vlrImpostoFederal.add(CalculaImpostoFederal(item, opOlhoNoImposto[0]));
+				vlrImpostoEstadual.add(totalItem.multiply(opOlhoNoImposto[0].getAliqEstadual()));
+				vlrImpostoMunicipal.add(totalItem.multiply(opOlhoNoImposto[0].getAliqMunicipal()));
+			});
+			
+			opDocFiscal.get().setValorImpostoFederal(vlrImpostoFederal);
+			opDocFiscal.get().setValorImpostoEstadual(vlrImpostoEstadual);
+			opDocFiscal.get().setValorImpostoMunicipal(vlrImpostoMunicipal);
+		}
+	}
+	
+	private BigDecimal CalculaImpostoFederal(DocumentoFiscalItem item, DeOlhoNoImposto opOlhoNoImposto) {
+		switch (item.getOrigem()) {
+			case NACIONAL:
+			case NACIONAL_CONTEUDO_IMPORTADO_MAIOR_40:
+			case NACIONAL_CONFORMIDADE_PROCESSOS:
+			case NACIONAL_CONTEUDO_IMPORTADO_MENOR_40:
+			case NACIONAL_CONTEUDO_IMPORTADO_MAIOR_70:
+			return totalItem(item).multiply(opOlhoNoImposto.getAliqFederalNacional());
+		default:
+			return totalItem(item).multiply(opOlhoNoImposto.getAliqFederalImportado());
+		}
+	}
+
+	
+	private BigDecimal totalItem(DocumentoFiscalItem item) {
+		return item.getValorUnitario().multiply(item.getQuantidade());
+	}
 	
 }
 
