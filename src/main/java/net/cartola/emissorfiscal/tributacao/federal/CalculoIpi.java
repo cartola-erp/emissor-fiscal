706c49c0bf75ca24f667ac8e00bfeeda82db1889
Robson Henrique <robsonhenrique16@hotmail.com>
2020-03-26 17:05:10 -0300

    [update] calc IPI

--- src/main/java/net/cartola/emissorfiscal/tributacao/federal/CalculoIpi.java
+++ src/main/java/net/cartola/emissorfiscal/tributacao/federal/CalculoIpi.java
@@ -1,8 +1,33 @@
 package net.cartola.emissorfiscal.tributacao.federal;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
+
+import net.cartola.emissorfiscal.documento.DocumentoFiscalItem;
+import net.cartola.emissorfiscal.tributacao.CalculoImposto;
+import net.cartola.emissorfiscal.tributacao.Imposto;
 
 @Service
 public class CalculoIpi {
-
+	
+	public CalculoImposto calculaIpi(DocumentoFiscalItem di, TributacaoFederal tributacao) {
+		CalculoImposto ipi = new CalculoImposto();
+		BigDecimal valorTotal = di.getQuantidade().multiply(di.getValorUnitario());
+		BigDecimal valorIpiBase = tributacao.getIpiBase().multiply(valorTotal);
+		BigDecimal valorIpi = valorIpiBase.multiply(tributacao.getIpiAliquota());
+		ipi.setImposto(Imposto.IPI);
+		ipi.setAliquota(tributacao.getIpiAliquota());
+		ipi.setBaseDeCalculo(valorIpiBase);
+//		pis.setOrdem(di.getId().intValue()); // -> mudar
+		ipi.setQuantidade(di.getQuantidade());
+		ipi.setValorUnitario(di.getValorUnitario());
+		ipi.setValor(valorIpi);
+		di.setIpiAliquota(tributacao.getIpiAliquota());
+		di.setIpiBase(valorIpiBase);
+		di.setIpiValor(valorIpi);
+		di.setIpiCst(tributacao.getIpiCst());
+		return ipi;
+	}
+	
 }
