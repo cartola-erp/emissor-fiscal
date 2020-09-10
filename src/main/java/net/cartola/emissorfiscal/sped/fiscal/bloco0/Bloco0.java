935ce6656e90edf9aa4ce5d8e80c702a3457e280
Robson Henrique Ramalho Costa <robson.costa@ag.local>
2020-09-10 15:26:14 -0300

    [update] retirando registros filhos (que são de NIVEL >= 3), pois os msms deveram ser preenchidos por seu respectivo pai

--- src/main/java/net/cartola/emissorfiscal/sped/fiscal/bloco0/Bloco0.java
+++ src/main/java/net/cartola/emissorfiscal/sped/fiscal/bloco0/Bloco0.java
@@ -10,6 +10,9 @@ import coffeepot.bean.wr.annotation.Record;
  * @author robson.costa
  * 
  * Bloco 0 - Identificação e referências (registros de tabelas)
+ * 
+ * <b> IMPORTANTE: </b> Deixar somente Registros, cujo o nivel seja até o "NÍVEL == 2", acima disso (registros filhos), 
+ * deveram ser preenchidos através de seu "REGISTRO PAI" 
  */
 @Record(fields = {
 	@Field(name = "reg0000"),
@@ -18,15 +21,15 @@ import coffeepot.bean.wr.annotation.Record;
 	@Field(name = "reg0015"),
 	@Field(name = "reg0100"),
 	@Field(name = "reg0150"),
-	@Field(name = "reg0175"),
+//	@Field(name = "reg0175"),
 	@Field(name = "reg0190"),
 	@Field(name = "reg0200"),
-	@Field(name = "reg0205"),
-	@Field(name = "reg0206"),
-	@Field(name = "reg0210"),
-	@Field(name = "reg0220"),
+//	@Field(name = "reg0205"),
+//	@Field(name = "reg0206"),
+//	@Field(name = "reg0210"),
+//	@Field(name = "reg0220"),
 	@Field(name = "reg0300"),
-	@Field(name = "reg0305"),
+//	@Field(name = "reg0305"),
 	@Field(name = "reg0400"),
 	@Field(name = "reg0450"),
 	@Field(name = "reg0460"),
@@ -36,13 +39,6 @@ import coffeepot.bean.wr.annotation.Record;
 })    
 public class Bloco0 {
 	
-	
-	/***
-	 * IMPORTANTE tenho que OLHAR NA DOCUMENTAÇÃO NOVAMENTE, pois ESQUECI que ALGUNS REGISTROS podem TER mais DE UMA VEZ num mesmo Documento do SPED FISCAL.
-	 * EXEMPLO o 0150, então ao INVES de SER OBJ tem que SER UMA LISTA; 
-	 * @Date 20/08/2020
-	 */
-	
 	private Reg0000AberturaArquivoDigital reg0000;
 	private Reg0001AberturaDoBloco reg0001;
 //	private Reg0002 reg0002;
@@ -50,15 +46,15 @@ public class Bloco0 {
 	private List<Reg0015> reg0015;
 	private Reg0100 reg0100;
 	private List<Reg0150> reg0150;
-	private List<Reg0175> reg0175;		// Filho do Reg0150
+//	private List<Reg0175> reg0175;		// Filho do Reg0150
 	private List<Reg0190> reg0190;		
 	private List<Reg0200> reg0200;
-	private List<Reg0205> reg0205;		// Filho do Reg0200
-	private Reg0206 reg0206;			// Filho do Reg0200
-	private List<Reg0210> reg0210;		// Filho do Reg0200
-	private List<Reg0220> reg0220;		// Filho do Reg0200
+//	private List<Reg0205> reg0205;		// Filho do Reg0200
+//	private Reg0206 reg0206;			// Filho do Reg0200
+//	private List<Reg0210> reg0210;		// Filho do Reg0200
+//	private List<Reg0220> reg0220;		// Filho do Reg0200
 	private List<Reg0300> reg0300;
-	private Reg0305 reg0305;			// Filho do Reg0300
+//	private Reg0305 reg0305;			// Filho do Reg0300
 	private List<Reg0400> reg0400;
 	private List<Reg0450> reg0450;
 	private List<Reg0460> reg0460;
@@ -114,14 +110,6 @@ public class Bloco0 {
 		this.reg0150 = reg0150;
 	}
 
-	public List<Reg0175> getReg0175() {
-		return reg0175;
-	}
-
-	public void setReg0175(List<Reg0175> reg0175) {
-		this.reg0175 = reg0175;
-	}
-
 	public List<Reg0190> getReg0190() {
 		return reg0190;
 	}
@@ -138,52 +126,12 @@ public class Bloco0 {
 		this.reg0200 = reg0200;
 	}
 
-	public List<Reg0205> getReg0205() {
-		return reg0205;
-	}
-
-	public void setReg0205(List<Reg0205> reg0205) {
-		this.reg0205 = reg0205;
-	}
-
-	public Reg0206 getReg0206() {
-		return reg0206;
-	}
-
-	public void setReg0206(Reg0206 reg0206) {
-		this.reg0206 = reg0206;
-	}
-
-	public List<Reg0210> getReg0210() {
-		return reg0210;
-	}
-
-	public void setReg0210(List<Reg0210> reg0210) {
-		this.reg0210 = reg0210;
-	}
-
-	public List<Reg0220> getReg0220() {
-		return reg0220;
-	}
-
-	public void setReg0220(List<Reg0220> reg0220) {
-		this.reg0220 = reg0220;
-	}
-
 	public List<Reg0300> getReg0300() {
 		return reg0300;
 	}
 
 	public void setReg0300(List<Reg0300> reg0300) {
 		this.reg0300 = reg0300;
-	}
-
-	public Reg0305 getReg0305() {
-		return reg0305;
-	}
-
-	public void setReg0305(Reg0305 reg0305) {
-		this.reg0305 = reg0305;
 	}
 
 	public List<Reg0400> getReg0400() {
