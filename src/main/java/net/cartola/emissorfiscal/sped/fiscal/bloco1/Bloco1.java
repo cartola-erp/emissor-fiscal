b42b5ce81610841deec0177e3b8b407ce28a8ce6
Robson Henrique Ramalho Costa <robson.costa@ag.local>
2020-09-15 15:06:46 -0300

    [add] Registros do bloco 1, que são usados pela AG

--- src/main/java/net/cartola/emissorfiscal/sped/fiscal/bloco1/Bloco1.java
+++ src/main/java/net/cartola/emissorfiscal/sped/fiscal/bloco1/Bloco1.java
@@ -17,35 +17,63 @@ import coffeepot.bean.wr.annotation.Field;
 	    @Field(name = "reg1001"),
 	    @Field(name = "reg1010"),
 
-	    //    @Field(name = "reg1100"),
-	    //    @Field(name = "reg1200"),
-	    //    @Field(name = "reg1300"),
-	    //    @Field(name = "reg1350"),
-	    //    @Field(name = "reg1390"),
-	    //    @Field(name = "reg1400"),
-	    //    @Field(name = "reg1500"),
-//	    @Field(name = "reg1600"),
-	    //    @Field(name = "reg1700"),
-	    //    @Field(name = "reg1800"),
-	    //    @Field(name = "reg1900"),
-	    @Field(name = "reg1990")
+//	   @Field(name = "reg1100"),
+//	   @Field(name = "reg1200"),
+//	   @Field(name = "reg1300"),
+//	   @Field(name = "reg1350"),
+//	   @Field(name = "reg1390"),
+//	   @Field(name = "reg1400"),
+//	   @Field(name = "reg1500"),
+//	   @Field(name = "reg1600"),
+//	   @Field(name = "reg1700"),
+//	   @Field(name = "reg1800"),
+//	   @Field(name = "reg1900"),
+	   @Field(name = "reg1990")
 })
 public class Bloco1 {
-	
-	  private Reg1001AberturaDoBloco1 reg1001;
-	  private Reg1010 reg1010;
 
-//	    private List<Reg1100> reg1100;
-//	    private List<Reg1200> reg1200;
-//	    private List<Reg1300> reg1300;
-//	    private List<Reg1350> reg1350;
-//	    private List<Reg1390> reg1390;
-//	    private List<Reg1400> reg1400;
-//	    private List<Reg1500> reg1500;
-//	    private List<Reg1600> reg1600;
-//	    private List<Reg1700> reg1700;
-//	    private Reg1800 reg1800;
-//	    private List<Reg1900> reg1900;
-	    private Reg1990EncerramentoDoBloco1 reg1990;
-	    
+	private Reg1001AberturaDoBloco1 reg1001;
+	private Reg1010 reg1010;
+
+	private Reg1990EncerramentoDoBloco1 reg1990;
+
+	/** GRUPO de que NÃO estavam presentes em nenhum arquivo do SPED (gerado pelo SAGE), que a Gabi me passou
+	 * 	Caso precise de algum, só modela todo o registro do grupo
+	 */
+//	private List<Reg1100> reg1100;
+//	private List<Reg1200> reg1200;
+//	private List<Reg1300> reg1300;
+//	private List<Reg1350> reg1350;
+//	private List<Reg1390> reg1390;
+//	private List<Reg1400> reg1400;
+//	private List<Reg1500> reg1500;
+//	private List<Reg1600> reg1600;
+//	private List<Reg1700> reg1700;
+//	private Reg1800 reg1800;
+//	private List<Reg1900> reg1900;
+	
+	public Reg1001AberturaDoBloco1 getReg1001() {
+		return reg1001;
+	}
+
+	public void setReg1001(Reg1001AberturaDoBloco1 reg1001) {
+		this.reg1001 = reg1001;
+	}
+
+	public Reg1010 getReg1010() {
+		return reg1010;
+	}
+
+	public void setReg1010(Reg1010 reg1010) {
+		this.reg1010 = reg1010;
+	}
+
+	public Reg1990EncerramentoDoBloco1 getReg1990() {
+		return reg1990;
+	}
+
+	public void setReg1990(Reg1990EncerramentoDoBloco1 reg1990) {
+		this.reg1990 = reg1990;
+	}
+	
 }
---
+++ src/main/java/net/cartola/emissorfiscal/sped/fiscal/bloco1/Reg1100.java
@@ -0,0 +1,20 @@
+package net.cartola.emissorfiscal.sped.fiscal.bloco1;
+
+import coffeepot.bean.wr.annotation.Field;
+import coffeepot.bean.wr.annotation.Record;
+
+/**
+ * 14/09/2020
+ * @author robson.costa
+ * 
+ */
+//@Record(fields = { 
+//		@Field(name = "reg", maxLength = 4),
+//
+//})
+public class Reg1100 {
+	
+	private final String reg = "1100";
+    
+    
+}
