9bcbf72beb378d7c202e98b35fe6d8dc09fcbd5c
Robson Henrique <robsonhenrique16@hotmail.com>
2020-09-28 17:03:59 -0300

    [add] getters/setters do contador, pela branch master

--- src/main/java/net/cartola/emissorfiscal/contador/DadosContador.java
+++ src/main/java/net/cartola/emissorfiscal/contador/DadosContador.java
@@ -27,4 +27,109 @@ public class DadosContador {
 	private String email;
 	private Long codMunicipio;
 	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getCrc() {
+		return crc;
+	}
+
+	public void setCrc(Long crc) {
+		this.crc = crc;
+	}
+
+	public Long getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(Long cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public Long getCep() {
+		return cep;
+	}
+
+	public void setCep(Long cep) {
+		this.cep = cep;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public int getNumImovel() {
+		return numImovel;
+	}
+
+	public void setNumImovel(int numImovel) {
+		this.numImovel = numImovel;
+	}
+
+	public String getDadosComplementares() {
+		return dadosComplementares;
+	}
+
+	public void setDadosComplementares(String dadosComplementares) {
+		this.dadosComplementares = dadosComplementares;
+	}
+
+	public String getBairroDoImovel() {
+		return bairroDoImovel;
+	}
+
+	public void setBairroDoImovel(String bairroDoImovel) {
+		this.bairroDoImovel = bairroDoImovel;
+	}
+
+	public String getNumTelefone() {
+		return numTelefone;
+	}
+
+	public void setNumTelefone(String numTelefone) {
+		this.numTelefone = numTelefone;
+	}
+
+	public String getNumFax() {
+		return numFax;
+	}
+
+	public void setNumFax(String numFax) {
+		this.numFax = numFax;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getCodMunicipio() {
+		return codMunicipio;
+	}
+
+	public void setCodMunicipio(Long codMunicipio) {
+		this.codMunicipio = codMunicipio;
+	}
+	
+	
 }
