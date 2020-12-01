7a27b757e2359e89acd6f085b4b6f1f8579bc4bb
Robson Henrique <robsonhenrique16@hotmail.com>
2020-12-01 15:53:01 -0300

    [add] script para inserir trib fede compraVenda de sucata bateria

---
+++ doc/scripts/cadastros-tributacoes-federais/20201201insertTribFede_CompraVendaSucataBateria.sql
@@ -0,0 +1,6 @@
+	
+	-- 73 | COMPRA DE SUCATA PARA COMERCIALIZACAO | NCM = 85481010| EX = 0
+	INSERT INTO `trib_fede` (cofins_aliq, cofins_base, cofins_cst, finalidade, ipi_aliq, ipi_base, ipi_cst, mens, pis_aliq, pis_base, pis_cst, regime_tributario, oper_id, ncm_id) 
+		VALUES (0.000000, 0.000000, 98, 'CONSUMO', 0.000000,0.000000, 98, 'Tributacao Cadastrada - 01/12 - VIA SCRIPT', 0.000000, 0.000000, 98,'NORMAL',73, 9902);	
+
+	
