c570a615197bdea85c3264f29cd1c36b2e08b808
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-03-30 16:36:28 -0300

    [update] mudando o email de envio dos calculo para fiscal

--- doc/scripts/20210311InsertIntoTrib_esta_guia_CompraParaComercia.sql
+++ doc/scripts/20210311InsertIntoTrib_esta_guia_CompraParaComercia.sql
@@ -178,3 +178,31 @@ INSERT INTO trib_esta_guia
     (icms_aliq_interna_dest,  icms_aliq, icms_iva, mens, produto_origem, tipo_guia, esta_dest_id, esta_orig_id, ncm_id, oper_id)  
     (SELECT 0.18, 0.12, 0.7148, "Inserido Via Script 10/03/2021", 8, "GARE_ICMS", 26, 8, ncm_id, 33 FROM trib_esta 
          WHERE icms_cst = 60 AND oper_id = 1);
+		 
+		 
+		 
+-- ============================================================================================================================================================
+-- --------------------------------------------------- ABAIXO está somente os NCMS que a GABI já confirmou ---------------------------------------------------------------
+-- ============================================================================================================================================================
+
+
+
+select * from trib_Esta_guia where ncm_id not in 
+(select ncm_id from ncms where nume 
+IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930));
+
+-- Deletando todas as tributações, que não tem nenhum dos ncms acima
+delete from trib_Esta_guia where ncm_id not in 
+(select ncm_id from ncms where nume 
+IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930));
+
+
+
+
+
+
+
+
+
+
+
