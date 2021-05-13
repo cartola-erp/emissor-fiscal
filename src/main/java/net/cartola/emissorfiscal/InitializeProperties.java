fbf700752fca69ebc58155389c47e8d18de8ff5b
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-05-13 15:28:51 -0300

    [update] configurando o secret manager para a chave do sendgrid

--- src/main/java/net/cartola/emissorfiscal/InitializeProperties.java
+++ src/main/java/net/cartola/emissorfiscal/InitializeProperties.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import net.cartola.emissorfiscal.properties.GuiaGareEmailProperties;
-import net.cartola.emissorfiscal.properties.SendgridApiProperties;
 
 /**
  * @date 18 de mar. de 2021
@@ -20,10 +19,4 @@ public class InitializeProperties {
 		return new GuiaGareEmailProperties();
 	}
 	
-	
-	@Bean
-	@ConfigurationProperties(value = "sendgrid-api")
-	public SendgridApiProperties sendgridApiProperties() {
-		return new SendgridApiProperties();
-	}
 }
