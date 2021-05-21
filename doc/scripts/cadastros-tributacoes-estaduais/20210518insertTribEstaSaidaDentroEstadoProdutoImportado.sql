5f1cd2a1f8c9981d8b863505a781c0daea9b0e1e
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-05-21 10:05:51 -0300

    {update] scripts para adicionar tributacao dentro do estado

--- doc/scripts/cadastros-tributacoes-estaduais/20210518insertTribEstaSaidaDentroEstadoProdutoImportado.sql
+++ doc/scripts/cadastros-tributacoes-estaduais/20210518insertTribEstaSaidaDentroEstadoProdutoImportado.sql
@@ -1,34 +1,54 @@
-
+-- ==================== is_prod_impor == TRUE
 
 -- ESSES que são transferência de saldo, terei que montar alguma lógica, para, para ir "calcular" somente o VALOR ICMS que está sendo transferido/creditado
 -- E além disso acrescentar a quantidade 1, no "item"
 
 	-- 13 | TRANSFERENCIA DE SALDO DEVEDOR ART.98 DO RICMS
 		INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, 
-							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id ) 
+							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
 		VALUES (199900, 5605, 0.000000, 'CONSUMO', 0.000000, 0.000000, 0.000000, 90, 0.000000, 0.000000, 
-		'TRANSFERENCIA DE SALDO DEVEVDOR DO ICMS DE R$ %s - APURACAO MES DE %s', 'NORMAL', 0, 26,  26, 11848, 13);	
+		'TRANSFERENCIA DE SALDO DEVEVDOR DO ICMS DE R$ %s - APURACAO MES DE %s', 'NORMAL', 0, 26,  26, 11848, 13, true);	
 
 	-- 14 | TRANSFERENCIA DE SALDO CREDOR  ART.98 DO RICMS |
 		INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, 
 							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id ) 
 		VALUES (199900, 5602, 0.000000, 'CONSUMO', 0.000000, 0.000000, 0.000000, 90, 0.000000, 0.000000, 
-		'TRANSFERENCIA DE SALDO CREDOR DO ICMS DE R$ %s - APURACAO MES DE %s', 'NORMAL', 0, 26,  26, 11848, 13);	
-
+		'TRANSFERENCIA DE SALDO CREDOR DO ICMS DE R$ %s - APURACAO MES DE %s', 'NORMAL', 0, 26,  26, 11848, 13, true);	
 -- ================================================================================================================================================================
+	
+	-- 1 | VENDA 	(Cadastrando VENDA e TRANSFERENCIA para CASO o PRODUTO SEJA IMPORTADO)
+-- ================================================================================================================================================================
+	INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, mens, regime_tributario, 
+		cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
+	(select cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, '', regime_tributario, 
+		cod_anp, esta_dest_id, esta_orig_id, ncm_id, 1, true FROM trib_esta where oper_id = 1 and is_prod_impor = false);
+	
+	
+	-- 4 | TRANSFERENCIA
+		
+	INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, mens, regime_tributario, 
+		cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
+	(select cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, '', regime_tributario, 
+		cod_anp, esta_dest_id, esta_orig_id, ncm_id, 4, true FROM trib_esta where oper_id = 4);
+	
+-- ================================================================================================================================================================
+-- ================================================================================================================================================================
+-- ================================================================================================================================================================
+
+
 
 	-- 45 | VENDA DE PRODUTO PARA  ENTREGA FUTURA
 -- -----------------------------------------------------------------------------------------------------------------------------------------------------
 	INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, 
-							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id ) 
+							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
 		(select cest, 5922, fcp_aliq, finalidade, 0.000000, 0.000000, 0.000000, 90, 0.000000, 0.000000, 
-				'', regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, 45 FROM trib_esta where oper_id = 1 and is_prod_impor = false);
+				'', regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, 45, true FROM trib_esta where oper_id = 1 and is_prod_impor = true);
 	
 	-- 45 | VENDA DE PRODUTO PARA  ENTREGA FUTURA Cadastrando para os outros ncms (que PROVAVELMENTE não SÃO de AUTOPEÇAS)
 	INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, 
-							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id )
+							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor )
 		(select 199900, 5922, 0, "CONSUMO", 0.000000, 0.000000, 0.000000, 90, 0.000000, 0.000000, 
-				'', "NORMAL", 0, 26, 26, ncm_id, 45 FROM ncms where ncm_id not in (select ncm_id from trib_esta where oper_id = 45 ));
+				'', "NORMAL", 0, 26, 26, ncm_id, 45, true FROM ncms where ncm_id not in (select ncm_id from trib_esta where oper_id = 45 and is_prod_impor = true));
 -- ---------------------------------------------- Para Quando o produto for importado (dentro do estado é a mesma tributacao -------------------------------	
 	
 	
@@ -40,27 +60,28 @@
 	-- 46 | REMESSA VINCULADA A VENDA DE ENTREGA FUTURA
 -- -----------------------------------------------------------------------------------------------------------------------------------------------------
 	INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, 
-							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id ) 
+							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
 		(select cest, 5117, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, 
-				'', regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, 46 FROM trib_esta where oper_id = 1 and is_prod_impor = false);
+				'', regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, 46, true FROM trib_esta where oper_id = 1 and is_prod_impor = true);
 
 
 -- -----------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-
+	
 	-- 67 | TRANSFERENCIA DE MATERIAL DE USO OU CONSUMO (CST 41 | zerado)
 -- -----------------------------------------------------------------------------------------------------------------------------------------------------
 	INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, 
-							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id ) 
+							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
 		(select cest, 5557, fcp_aliq, finalidade, 0.000000, 0.000000, 0.000000, 41, 0.000000, 0.000000, 
 				'NOTA FISCAL EMITIDA CONFORME O ARTIGO 7 INCISO XV DO REGULAMENTO DO ICMS DE SP', regime_tributario, 
-				cod_anp, esta_dest_id, esta_orig_id, ncm_id, 67 FROM trib_esta where oper_id = 1 and is_prod_impor = false);
+				cod_anp, esta_dest_id, esta_orig_id, ncm_id, 67, true FROM trib_esta where oper_id = 1 and is_prod_impor = true);
 	
 	-- 67 | TRANSFERENCIA DE MATERIAL DE USO OU CONSUMO (CST 41 | zerado) Cadastrando para os outros ncms (que PROVAVELMENTE não SÃO de AUTOPEÇAS)
 	INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, 
-							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id )
+							mens, regime_tributario, cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor )
 		(select 199900, 5557, 0, "CONSUMO", 0.000000, 0.000000, 0.000000, 41, 0.000000, 0.000000, 
 				'NOTA FISCAL EMITIDA CONFORME O ARTIGO 7 INCISO XV DO REGULAMENTO DO ICMS DE SP', "NORMAL", 
-				0, 26, 26, ncm_id, 67 FROM ncms where ncm_id not in (select ncm_id from trib_esta where oper_id = 67));
+				0, 26, 26, ncm_id, 67, true FROM ncms where ncm_id not in (select ncm_id from trib_esta where oper_id = 67 and is_prod_impor = true));
 -- -----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
