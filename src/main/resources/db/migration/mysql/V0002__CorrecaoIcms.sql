3c10e3a1a5a25ebac762de4ef6587bcd2b7607d3
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-05-26 14:33:14 -0300

    [update] script de correcao do icms

---
+++ src/main/resources/db/migration/mysql/V0002__CorrecaoIcms.sql
@@ -0,0 +1,75 @@
+
+
+-- =========================================================================================================================================================
+-- =========================================================================================================================================================
+-- =========================================================================================================================================================
+
+
+-- Deixando o campo "mens", vazio, pois será usado como info complementar;
+update trib_esta set mens = "";
+
+-- O script abaixo é para atualizar o ICMS na VENDA e TRANSFERENCIA, para os NCMS (73181500, 73181600, 76161000, 73182400, 84814000, 84818099)
+-- Da CST 60 --> p/ 00 (que é tributado) 
+
+
+SELECT * FROM trib_esta where ncm_id in (select ncm_id from ncms where nume = 38112190);
+
+update trib_esta SET cod_anp = 740101006 where ncm_id in (select ncm_id from ncms where nume = 38112190);
+
+	
+
+-- RODAR NO EMISSORFISCAL (CORRIGINDO os NCMS que estão na CST 60 para a CST 00)
+-- 1 - VENDA
+SELECT * FROM  emissorfiscal.trib_esta WHERE ncm_id IN ( 
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN (73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300 )
+) and oper_id = 1 ;
+
+UPDATE emissorfiscal.trib_esta t 
+		SET t.fcp_aliq = 0.000000,
+		t.icms_aliq =  0.180000,
+		t.icms_aliq_dest = 0.000000,
+		t.icms_base = 1.000000,
+		t.icms_cst = 0,
+		t.icms_iva = 1.000000,
+		t.icms_st_aliq = 0.000000,
+		t.mens =  '',
+		t.cfop = 5102 
+WHERE t.ncm_id IN ( 
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300 )
+) and t.oper_id = 1 AND  t.icms_cst = 60;
+
+
+-- 4 - TRANSFERENCIA
+SELECT * FROM  emissorfiscal.trib_esta WHERE ncm_id IN ( 
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN (73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300 )
+) and oper_id = 4 ;
+
+UPDATE emissorfiscal.trib_esta t 
+		SET t.fcp_aliq = 0.000000,
+		t.icms_aliq =  0.180000,
+		t.icms_aliq_dest = 0.000000,
+		t.icms_base = 1.000000,
+		t.icms_cst = 0,
+		t.icms_iva = 1.000000,
+		t.icms_st_aliq = 0.000000,
+		t.mens =  '',
+		t.cfop = 5152 
+WHERE t.ncm_id IN ( 
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300 )
+) and t.oper_id = 4 AND  t.icms_cst = 60;
+
+
+
+
+-- ATUALIZANDO CFOP de OLEO
+
+-- VENDA
+select * from emissorfiscal.trib_esta t where cod_anp != 0 and oper_Id = 1;
+
+update emissorfiscal.trib_esta set cfop = 5656 where cod_anp != 0 and oper_Id = 1;
+
+
+-- TRANSFERENCIA
+select * from emissorfiscal.trib_esta t where cod_anp != 0 and oper_Id = 4;
+
+update emissorfiscal.trib_esta set cfop = 5659 where cod_anp != 0 and oper_Id = 4;
