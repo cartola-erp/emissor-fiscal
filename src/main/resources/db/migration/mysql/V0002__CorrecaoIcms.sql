de30fd5818b8590b2a362ec40be352448ac473c2
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-05-27 17:19:21 -0300

    [udapte] acrescentando correcao de aliquotas de 12% para 13,3% em alguns ncms

--- src/main/resources/db/migration/mysql/V0002__CorrecaoIcms.sql
+++ src/main/resources/db/migration/mysql/V0002__CorrecaoIcms.sql
@@ -73,3 +73,36 @@ update emissorfiscal.trib_esta set cfop = 5656 where cod_anp != 0 and oper_Id = 
 -- select * from emissorfiscal.trib_esta t where cod_anp != 0 and oper_Id = 4;
 
 update emissorfiscal.trib_esta set cfop = 5659 where cod_anp != 0 and oper_Id = 4;
+
+
+-- Atualizando ALIQUOTAS de 12% p/ 13,3%
+
+UPDATE emissorfiscal.trib_esta t 
+		SET t.fcp_aliq = 0.000000,
+		t.icms_aliq =  0.133000,
+		t.icms_aliq_dest = 0.000000,
+		t.icms_base = 1.000000,
+		t.icms_cst = 0,
+		t.icms_iva = 1.000000,
+		t.icms_st_aliq = 0.000000,
+		t.mens =  '',
+		t.cfop = 5102 
+WHERE t.ncm_id IN ( 
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(84818021, 90271000, 85432000)
+) and t.oper_id = 1;
+
+
+-- 4 - TRANSFERENCIA
+UPDATE emissorfiscal.trib_esta t 
+		SET t.fcp_aliq = 0.000000,
+		t.icms_aliq =  0.133000,
+		t.icms_aliq_dest = 0.000000,
+		t.icms_base = 1.000000,
+		t.icms_cst = 0,
+		t.icms_iva = 1.000000,
+		t.icms_st_aliq = 0.000000,
+		t.mens =  '',
+		t.cfop = 5152 
+WHERE t.ncm_id IN ( 
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(84818021, 90271000, 85432000)
+) and t.oper_id = 4;
