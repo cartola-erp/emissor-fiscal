41e51874b18bc71af41dfabf48f22197399b2e4a
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-05-31 13:24:57 -0300

    {add] deploy.bat

---
+++ deploy.bat
@@ -0,0 +1,1 @@
+mvn clean install -Pproducao -DskipTests && rundll32.exe cmdext.dll,MessageBeepStub && pause && cls && mvn package -Pproducao -DskipTests && rundll32.exe cmdext.dll,MessageBeepStub && pause && cls && mvn appengine:deploy -Pproducao -DskipTests && rundll32.exe cmdext.dll,MessageBeepStub && pause
