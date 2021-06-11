70fc73a21bb807db16afdd83245e6c8e9e2723b5
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-06-11 17:07:23 -0300

    [update] adicionando comentario na classe

--- src/main/java/net/cartola/emissorfiscal/sped/fiscal/RegistroAnalitico.java
+++ src/main/java/net/cartola/emissorfiscal/sped/fiscal/RegistroAnalitico.java
@@ -6,6 +6,13 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
+ * 
+ * Essa classe é (pode ser) a SuperClasse dos registros: C190, C320, C390, C490, C590, C690, C790, C850, C890, D190, D300, D390, D410, D590, D690, D696;
+ *
+ * Atualmente ela só é SuperClasse dos dos Registros que escrituramos, que os Registros:
+ * 
+ * C190, C590, C850, D190, D590
+ *
  * @data 11 de jun. de 2021
  * @author robson.costa
  */
