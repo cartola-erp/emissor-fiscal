dacce0aa48e8742d39a5bad9a8b4ef19c8b27dab
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-07-07 11:17:26 -0300

    [update] corrigindo mas ncms na tributação de icms

--- doc/scripts/cadastros-tributacoes-estaduais/20210517correcaoIcms.sql
+++ doc/scripts/cadastros-tributacoes-estaduais/20210517correcaoIcms.sql
@@ -4,17 +4,19 @@
 -- =========================================================================================================================================================
 -- FINALIDADES
 
+-- PRODUTOS TRIBUTACAO
 
--- 2 - SUBSTITUIÇÃO TRIBUTÁRIA
--- 1 - NORMAL
+	-- 1 - NORMAL
+	-- 2 - SUBSTITUIÇÃO TRIBUTÁRIA
+
 
 -- 
 -- Todos os produtos que tiverem a classe fiscal abaixa e forem TRIBUTADOS como "normal" serão atualizados a TRIBUTACAO para "ST" (Substituição Tributária) (CST 60 - Base E Aliq ZERADA)
 -- 
 
-SELECT * FROM autogeral.produtos_dbf WHERE classe_fiscal IN (40091100, 90262090) AND tributacao_codigo = 1;
+SELECT * FROM autogeral.produtos_dbf WHERE classe_fiscal IN (40091100, 90262090, 87089100) AND tributacao_codigo = 1;
 
-update autogeral.produtos_dbf set tributacao_codigo = 2 WHERE classe_fiscal IN (40091100, 90262090) AND tributacao_codigo = 1;
+update autogeral.produtos_dbf set tributacao_codigo = 2 WHERE classe_fiscal IN (40091100, 90262090, 87089100) AND tributacao_codigo = 1;
 
 
 -- PS: Os ncms ACIMA no emissorfiscal, já estão como ST, e os abaixo tbm.
@@ -24,9 +26,9 @@ update autogeral.produtos_dbf set tributacao_codigo = 2 WHERE classe_fiscal IN (
 -- Todos os produtos que tiverem os NCMs e que estiverem cadastrados como ST (tributacao_codigo = 2) serão atualizados a TRIBUTACAO do ICMS para "NORMAL" (CST 00)
 -- 
 
-SELECT * FROM autogeral.produtos_dbf WHERE classe_fiscal IN (73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300 ) AND tributacao_codigo = 2 ;
+SELECT * FROM autogeral.produtos_dbf WHERE classe_fiscal IN (73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300, 39269090, 32141010 ) AND tributacao_codigo = 2 ;
 
-update autogeral.produtos_dbf set tributacao_codigo = 1 WHERE classe_fiscal IN (73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300 ) AND tributacao_codigo = 2 ;
+update autogeral.produtos_dbf set tributacao_codigo = 1 WHERE classe_fiscal IN (73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300, 39269090, 32141010 ) AND tributacao_codigo = 2 ;
 
 -- =========================================================================================================================================================
 -- =========================================================================================================================================================
@@ -46,11 +48,12 @@ SELECT * FROM trib_esta where ncm_id in (select ncm_id from ncms where nume = 38
 update trib_esta SET cod_anp = 740101006 where ncm_id in (select ncm_id from ncms where nume = 38112190);
 
 	
-
--- RODAR NO EMISSORFISCAL (CORRIGINDO os NCMS que estão na CST 60 para a CST 00)
+-- =========================================================================================================================================================
+-- ###############################	RODAR NO EMISSORFISCAL (CORRIGINDO os NCMS que estão na CST 60 para a CST 00)	########################################
+-- =========================================================================================================================================================
 -- 1 - VENDA
 SELECT * FROM  emissorfiscal.trib_esta WHERE ncm_id IN ( 
-	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN (73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300, 73182200 )
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN (73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300, 73182200, 39269090 )
 ) and oper_id = 1 ;
 
 UPDATE emissorfiscal.trib_esta t 
@@ -64,13 +67,13 @@ UPDATE emissorfiscal.trib_esta t
 		t.mens =  '',
 		t.cfop = 5102 
 WHERE t.ncm_id IN ( 
-	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300, 73182200 )
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300, 73182200, 39269090 )
 ) and t.oper_id = 1 AND  t.icms_cst = 60;
 
 
 -- 4 - TRANSFERENCIA
 SELECT * FROM  emissorfiscal.trib_esta WHERE ncm_id IN ( 
-	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN (73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300, 73182200 )
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN (73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300, 73182200, 39269090 )
 ) and oper_id = 4 ;
 
 UPDATE emissorfiscal.trib_esta t 
@@ -84,14 +87,63 @@ UPDATE emissorfiscal.trib_esta t
 		t.mens =  '',
 		t.cfop = 5152 
 WHERE t.ncm_id IN ( 
-	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300, 73182200 )
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(73181500, 73181600, 76161000, 73182400, 84814000, 84818099, 73182300, 73182200, 39269090 )
 ) and t.oper_id = 4 AND  t.icms_cst = 60;
+-- =========================================================================================================================================================
+
+
+
+-- =========================================================================================================================================================
+-- ###############################	RODAR NO EMISSORFISCAL (CORRIGINDO os NCMS que estão na CST 00 para a CST 60)	########################################
+-- =========================================================================================================================================================
+-- 1 - VENDA
+SELECT * FROM  emissorfiscal.trib_esta WHERE ncm_id IN ( 
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN (87089100)
+) and oper_id = 1 ;
+
+UPDATE emissorfiscal.trib_esta t 
+		SET t.fcp_aliq = 0.000000,
+		t.icms_aliq =  0.000000,
+		t.icms_aliq_dest = 0.000000,
+		t.icms_base = 1.000000,
+		t.icms_cst = 60,
+		t.icms_iva = 1.000000,
+		t.icms_st_aliq = 0.000000,
+		t.mens =  '',
+		t.cfop = 5405 
+WHERE t.ncm_id IN ( 
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(87089100)
+) and t.oper_id = 1 AND  t.icms_cst = 0;
+
+
+-- 4 - TRANSFERENCIA
+SELECT * FROM  emissorfiscal.trib_esta WHERE ncm_id IN ( 
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN (87089100 )
+) and oper_id = 4 ;
+
+UPDATE emissorfiscal.trib_esta t 
+		SET t.fcp_aliq = 0.000000,
+		t.icms_aliq =  0.000000,
+		t.icms_aliq_dest = 0.000000,
+		t.icms_base = 1.000000,
+		t.icms_cst = 60,
+		t.icms_iva = 1.000000,
+		t.icms_st_aliq = 0.000000,
+		t.mens =  '',
+		t.cfop = 5409
+WHERE t.ncm_id IN ( 
+	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(87089100 )
+) and t.oper_id = 4 AND  t.icms_cst = 0;
+-- =========================================================================================================================================================
 
 
 
 
--- ATUALIZANDO CFOP de OLEO
 
+
+-- =========================================================================================================================================================
+-- ########################################################	 	ATUALIZANDO a CFOP de OLEO		############################################################
+-- =========================================================================================================================================================
 -- VENDA
 select * from emissorfiscal.trib_esta t where cod_anp != 0 and oper_Id = 1;
 
@@ -102,11 +154,13 @@ update emissorfiscal.trib_esta set cfop = 5656 where cod_anp != 0 and oper_Id = 
 select * from emissorfiscal.trib_esta t where cod_anp != 0 and oper_Id = 4;
 
 update emissorfiscal.trib_esta set cfop = 5659 where cod_anp != 0 and oper_Id = 4;
+-- =========================================================================================================================================================
 
 
--- Atualizando ALIQUOTAS de 12% p/ 13,3%
 
-
+-- =========================================================================================================================================================
+-- ##################################################	 Atualizando ALIQUOTAS de 12% p/ 13,3%		###########################################################
+-- =========================================================================================================================================================
 -- 1 - VENDA
 SELECT * FROM  emissorfiscal.trib_esta WHERE ncm_id IN ( 
 	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN (84818021, 90271000, 85432000)
@@ -146,3 +200,4 @@ UPDATE emissorfiscal.trib_esta t
 WHERE t.ncm_id IN ( 
 	SELECT ncm_id FROM emissorfiscal.ncms WHERE nume IN(84818021, 90271000, 85432000)
 ) and t.oper_id = 4;
+-- =========================================================================================================================================================
