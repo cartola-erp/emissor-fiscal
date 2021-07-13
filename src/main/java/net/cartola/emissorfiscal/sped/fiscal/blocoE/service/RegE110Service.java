350ed320fd4043f299795bfdd05de4c857688526
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-07-13 17:12:56 -0300

    [update] chamando a montagem do REG E111, antes de preencher os campos do REG E110

--- src/main/java/net/cartola/emissorfiscal/sped/fiscal/blocoE/service/RegE110Service.java
+++ src/main/java/net/cartola/emissorfiscal/sped/fiscal/blocoE/service/RegE110Service.java
@@ -9,13 +9,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import net.cartola.emissorfiscal.documento.IndicadorDeOperacao;
+import net.cartola.emissorfiscal.model.sped.fiscal.icms.propria.SpedFiscalRegE110Service;
 import net.cartola.emissorfiscal.sped.fiscal.MovimentoMensalIcmsIpi;
 import net.cartola.emissorfiscal.sped.fiscal.OutrasObrigacoesEAjustes;
 import net.cartola.emissorfiscal.sped.fiscal.RegistroAnalitico;
 import net.cartola.emissorfiscal.sped.fiscal.blocoE.RegE110;
+import net.cartola.emissorfiscal.sped.fiscal.blocoE.RegE111;
 
 /**
  * @data 11 de jun. de 2021
@@ -23,10 +26,18 @@ import net.cartola.emissorfiscal.sped.fiscal.blocoE.RegE110;
  */
 @Service
 class RegE110Service {
+	
+	@Autowired
+	private RegE111Service reg111Service;
+	
+	@Autowired
+	private SpedFiscalRegE110Service spedFiscRegE110Service;
 
 	public RegE110 montaGrupoRegE110(MovimentoMensalIcmsIpi movimentosIcmsIpi) {
 		// TODO Auto-generated method stub
 		RegE110 regE110 = new RegE110();
+		
+		List<RegE111> listRegE111 = reg111Service.montarGrupoRegE111(movimentosIcmsIpi);
 		
 		regE110.setVlTotDebitos(calcularVlTotalDebitos(movimentosIcmsIpi.getMapRegistroAnaliticoPorTipoOperacao()));	/** CAMPO 02  **/ 
 		regE110.setVlAjDebitos(calcularVlAjusteDebitos(movimentosIcmsIpi.getSetOutrasObrigacoesEAjustes()));			/** CAMPO 03  **/ 
