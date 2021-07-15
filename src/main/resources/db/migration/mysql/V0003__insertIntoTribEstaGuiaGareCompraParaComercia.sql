329b36b7957d344f827e9a0b6a9ef9e093cf7ee4
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-07-15 10:08:20 -0300

    {update] acrescentando mais um ncm nos scripts das guias de SC

--- src/main/resources/db/migration/mysql/V0003__insertIntoTribEstaGuiaGareCompraParaComercia.sql
+++ src/main/resources/db/migration/mysql/V0003__insertIntoTribEstaGuiaGareCompraParaComercia.sql
@@ -8,7 +8,7 @@
 INSERT INTO trib_esta_guia  
     (icms_Aliq_interna_dest,  icms_Aliq, icms_iva, mens, is_prod_impor, tipo_guia, esta_dest_id, esta_orig_id, ncm_id, oper_id)  
     (SELECT 0.18, 0.04, 0.7148, "", 1, "GARE_ICMS", 26, 24, ncm_id, 33 FROM ncms 
-         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300 ));
+         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300, 84219999 ));
 	
 	
 -- DE SC p/ SP - Aliq == 12% (Produto NACIONAL)
@@ -16,7 +16,7 @@ INSERT INTO trib_esta_guia
 INSERT INTO trib_esta_guia  
     (icms_Aliq_interna_dest,  icms_Aliq, icms_iva, mens, is_prod_impor, tipo_guia, esta_dest_id, esta_orig_id, ncm_id, oper_id)  
     (SELECT 0.18, 0.12, 0.7148, "", 0, "GARE_ICMS", 26, 24, ncm_id, 33 FROM ncms 
-         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300 ));
+         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300, 84219999 ));
 		
 				
 -- ============================================================================================================================================================
@@ -28,7 +28,7 @@ INSERT INTO trib_esta_guia
 INSERT INTO trib_esta_guia  
     (icms_Aliq_interna_dest,  icms_Aliq, icms_iva, mens, is_prod_impor, tipo_guia, esta_dest_id, esta_orig_id, ncm_id, oper_id)  
     (SELECT 0.18, 0.04, 0.7148, "", 1, "GARE_ICMS", 26, 12, ncm_id, 33 FROM ncms 
-         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300 ));
+         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300, 84219999 ));
 	
 		
 -- DE MS p/ SP - Aliq == 12% (Produto NACIONAL)
@@ -36,7 +36,7 @@ INSERT INTO trib_esta_guia
 INSERT INTO trib_esta_guia  
     (icms_Aliq_interna_dest,  icms_Aliq, icms_iva, mens, is_prod_impor, tipo_guia, esta_dest_id, esta_orig_id, ncm_id, oper_id)  
     (SELECT 0.18, 0.12, 0.7148, "", 0, "GARE_ICMS", 26, 12, ncm_id, 33 FROM ncms 
-         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300 ));
+         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300, 84219999 ));
 	
 
 -- ============================================================================================================================================================
@@ -48,14 +48,14 @@ INSERT INTO trib_esta_guia
 INSERT INTO trib_esta_guia  
     (icms_Aliq_interna_dest,  icms_Aliq, icms_iva, mens, is_prod_impor, tipo_guia, esta_dest_id, esta_orig_id, ncm_id, oper_id)  
     (SELECT 0.18, 0.04, 0.7148, "", 1, "GARE_ICMS", 26, 8, ncm_id, 33 FROM ncms 
-         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300 ));
+         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300, 84219999 ));
 			
 -- DE ES p/ SP - Aliq == 12% (Produto NACIONAL)
 		
 INSERT INTO trib_esta_guia  
     (icms_Aliq_interna_dest,  icms_Aliq, icms_iva, mens, is_prod_impor, tipo_guia, esta_dest_id, esta_orig_id, ncm_id, oper_id)  
     (SELECT 0.18, 0.12, 0.7148, "", 0, "GARE_ICMS", 26, 8, ncm_id, 33 FROM ncms 
-         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300 ));
+         WHERE nume IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300, 84219999 ));
 				 
 		 		 
 -- ============================================================================================================================================================
@@ -66,12 +66,12 @@ INSERT INTO trib_esta_guia
 
 select * from trib_esta_guia where ncm_id not in 
 (select ncm_id from ncms where nume 
-IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300));
+IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300, 84219999));
 
 -- Deletando todas as tributações, que não tem nenhum dos ncms acima
-delete from trib_esta_guia where ncm_id not in 
-(select ncm_id from ncms where nume 
-IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300));
+	-- delete from trib_esta_guia where ncm_id not in 
+	-- (select ncm_id from ncms where nume 
+	-- IN (87085099, 87083090, 84811000, 84123110, 84148019, 87089300, 84136019, 87087090, 84099190, 84099999, 84099930, 84099917, 84099959, 84212300, 84219999));
 
 
 
