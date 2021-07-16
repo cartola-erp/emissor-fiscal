288ed6318f4a4335c8777f829a6b206cb41d2583
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-07-16 11:47:05 -0300

    [update] populando mapa por tipo ajuste e deducao

--- src/main/java/net/cartola/emissorfiscal/model/sped/fiscal/tabelas/Tabela511AjusteApuracaoIcmsSpService.java
+++ src/main/java/net/cartola/emissorfiscal/model/sped/fiscal/tabelas/Tabela511AjusteApuracaoIcmsSpService.java
@@ -1,6 +1,7 @@
 package net.cartola.emissorfiscal.model.sped.fiscal.tabelas;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -19,15 +20,31 @@ public class Tabela511AjusteApuracaoIcmsSpService {
 	@Autowired
 	private Tabela511AjusteApuracaoIcmsSpRepository tabelaAjustIcmsSpRepo;
 	
+//	private static Map<TipoAjusteTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> mapTabela511PorTipoAjust;
 	private static Map<TipoAjusteTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> mapTabela511PorTipoAjust;
 	
-	private static Map<TipoAjusteTabela511, Map<TipoDeducaoTabela511, Set<Tabela511AjusteApuracaoIcmsSp>>> mapTabela511PorTipoAjusEDeducao;
+	private static Map<TipoAjusteTabela511, Map<TipoDeducaoTabela511, Set<Tabela511AjusteApuracaoIcmsSp>>> mapTabela511PorTipoAjustEDeducao;
 	
 	
 	public List<Tabela511AjusteApuracaoIcmsSp> findAll() {
 		return tabelaAjustIcmsSpRepo.findAll();
 	}
 	
+	
+	/**
+	 * Retorna uma lista com os códigos da Tabela 5.1.1 - "...Ajuste apuracao ICMS SP...", pelo {@link TipoAjusteTabela511}
+	 * 
+	 * @param tipoAjuste
+	 * @return
+	 */
+	public Set<Tabela511AjusteApuracaoIcmsSp> buscarPorTipoAjuste(TipoAjusteTabela511 tipoAjuste) {
+		Set<Tabela511AjusteApuracaoIcmsSp> listTabela511PorTipoAjust = null;
+		preencherMapTabela511PorTipoAjuste();
+		if (mapTabela511PorTipoAjust != null && mapTabela511PorTipoAjust.containsKey(tipoAjuste)) {
+			listTabela511PorTipoAjust = mapTabela511PorTipoAjust.get(tipoAjuste);
+		}
+		return (listTabela511PorTipoAjust != null) ? listTabela511PorTipoAjust : new HashSet<>() ;
+	}
 	
 	/**
 	 * Retorna uma lista com os códigos da Tabela 5.1.1 - "...Ajuste apuracao ICMS SP...", 
@@ -38,82 +55,123 @@ public class Tabela511AjusteApuracaoIcmsSpService {
 	 * @return
 	 */
 	public Set<Tabela511AjusteApuracaoIcmsSp> buscarPorTipoAjusteEDeducao(TipoAjusteTabela511 tipoAjuste, TipoDeducaoTabela511 tipoDeducao) {
-		Map<TipoAjusteTabela511, Map<TipoDeducaoTabela511, Set<Tabela511AjusteApuracaoIcmsSp>>> map;
-		Map<TipoAjusteTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> mapTabela511PorTipoAjuste = getMapTabela511PorTipoAjuste();
-
-		if (mapTabela511PorTipoAjuste != null && !mapTabela511PorTipoAjuste.isEmpty() && mapTabela511PorTipoAjusEDeducao != null && mapTabela511PorTipoAjusEDeducao.isEmpty()) {
-			populaMapTabela511PorTipoAjusteEDeducao(mapTabela511PorTipoAjuste);
+		Set<Tabela511AjusteApuracaoIcmsSp> setPorTipoAjusteAndTipoDeducao = null;
+		preencherMapTabela511PorTipoAjuste();
+		if (!isMapNullOrEmpty(mapTabela511PorTipoAjust) && isMapNullOrEmpty(mapTabela511PorTipoAjustEDeducao)) {
+			for (TipoAjusteTabela511 tipoAjusteNoMapa : mapTabela511PorTipoAjust.keySet()) {
+				Set<Tabela511AjusteApuracaoIcmsSp> setTbl511PorTipoAjuste = mapTabela511PorTipoAjust.get(tipoAjusteNoMapa);
+				preencherMapTabela511PorTipoAjustEDeducao(setTbl511PorTipoAjuste, tipoAjusteNoMapa);
+			}
+			
+			setPorTipoAjusteAndTipoDeducao = mapTabela511PorTipoAjustEDeducao.get(tipoAjuste).get(tipoDeducao);
 		}
-		
-		Set<Tabela511AjusteApuracaoIcmsSp> setTabela511AjusteApuracaoIcmsSp = mapTabela511PorTipoAjusEDeducao.get(tipoAjuste).get(tipoDeducao);
-		return setTabela511AjusteApuracaoIcmsSp;
+		return (setPorTipoAjusteAndTipoDeducao != null) ? setPorTipoAjusteAndTipoDeducao : new HashSet<>();
 	}
+
 	
-	
-	private void populaMapTabela511PorTipoAjusteEDeducao(Map<TipoAjusteTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> mapTabela511PorTipoAjuste) {
-		// TODO Auto-generated method stub
-		for (TipoAjusteTabela511 tipoAjuste : mapTabela511PorTipoAjuste.keySet()) {
-			Set<Tabela511AjusteApuracaoIcmsSp> setTabela511AjusteApuracao = mapTabela511PorTipoAjuste.get(tipoAjuste);
-			
-//			for (Tabela511AjusteApuracaoIcmsSp tabela511AjusteApuracaoIcmsSp : setTabela511AjusteApuracao) {
-//				// verifica tipo de deducao
-//			}
-			
-			setTabela511AjusteApuracao.stream().forEachOrdered(tbl511AjustApuracaoIcmsSp -> {
-				Character quartoCaracter = tbl511AjustApuracaoIcmsSp.getCodigoAjuste().charAt(3);
-				TipoDeducaoTabela511 tipoDeducao = getTipoDeducao(quartoCaracter);
-				
-				// popula o mapa de deducao
+	/**
+	 * Será preenchido o "mapTabela511PorTipoAjust" (caso o mesmo esteja vazio ou seja null)
+	 */
+	private void preencherMapTabela511PorTipoAjuste() {
+		if (mapTabela511PorTipoAjust == null || mapTabela511PorTipoAjust.isEmpty()) {
+			List<Tabela511AjusteApuracaoIcmsSp> listAjustesTabela511 = findAll();
+			listAjustesTabela511.stream().forEachOrdered(tbl511 -> {
+				TipoAjusteTabela511 tipoAjuste = getTipoAjuste(tbl511);
+				populaMapTabela511PorTipoAjuste(tbl511, tipoAjuste);
 			});
 		}
+	}
+
+	private void preencherMapTabela511PorTipoAjustEDeducao (Set<Tabela511AjusteApuracaoIcmsSp> setTbl511PorTipoAjuste, TipoAjusteTabela511 tipoAjusteNoMapa)  {
+//		Map<TipoAjusteTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> mapPorTipoAjuste = null;
+//		if (isMapNullOrEmpty(mapTabela511PorTipoAjustEDeducao)) {
+			Set<Tabela511AjusteApuracaoIcmsSp> setPorTipoAjuste = mapTabela511PorTipoAjust.get(tipoAjusteNoMapa);
+//			if (!isMapNull(mapPorTipoAjuste)) {
+			if (setPorTipoAjuste != null && !setPorTipoAjuste.isEmpty()) {
+				for (Tabela511AjusteApuracaoIcmsSp tbl511 : setPorTipoAjuste) {
+					populaMapTabela511PorTipoAjusteEDeducao(tbl511, tipoAjusteNoMapa);
+				}
+			}
+//		}
+	}
+	
+
+	private void populaMapTabela511PorTipoAjusteEDeducao(Tabela511AjusteApuracaoIcmsSp tbl511, TipoAjusteTabela511 tipoAjuste) {
+		// TODO Auto-generated method stub
+		if (isMapNullOrEmpty(mapTabela511PorTipoAjustEDeducao)) {
+			mapTabela511PorTipoAjustEDeducao = new HashMap<>();
+		}
+		TipoDeducaoTabela511 tipoDeducao = getTipoDeducao(tbl511);
+		if (mapTabela511PorTipoAjustEDeducao.containsKey(tipoAjuste) && tipoDeducao != null) {
+			Map<TipoDeducaoTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> mapPorTipoAjuste = mapTabela511PorTipoAjustEDeducao.get(tipoAjuste);
+			if (!isMapNull(mapPorTipoAjuste) && mapPorTipoAjuste.containsKey(tipoDeducao)) {
+				mapPorTipoAjuste.get(tipoDeducao).add(tbl511);
+			} else {
+				Map<TipoDeducaoTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> newMapPorTipoDeducao = new HashMap<>();
+				Set<Tabela511AjusteApuracaoIcmsSp> setPorDeducao = new HashSet<>();
+				setPorDeducao.add(tbl511);
+				newMapPorTipoDeducao.put(tipoDeducao, setPorDeducao);
+				if(mapTabela511PorTipoAjustEDeducao.containsKey(tipoAjuste)) {
+					Map<TipoDeducaoTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> mapPorDeducao = mapTabela511PorTipoAjustEDeducao.get(tipoAjuste);
+					if (mapPorDeducao.containsKey(tipoDeducao)) {
+						mapPorDeducao.get(tipoDeducao).add(tbl511);
+					} else {
+						mapTabela511PorTipoAjustEDeducao.get(tipoAjuste).put(tipoDeducao, setPorDeducao);
+					}
+				} else {
+					mapTabela511PorTipoAjustEDeducao.put(tipoAjuste, newMapPorTipoDeducao);
+				}
+			}
+		} else {
+			Map<TipoDeducaoTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> newMapPorTipoDeducao = new HashMap<>();
+			Set<Tabela511AjusteApuracaoIcmsSp> setPorDeducao = new HashSet<>();
+			setPorDeducao.add(tbl511);
+			newMapPorTipoDeducao.put(tipoDeducao, setPorDeducao);
+			mapTabela511PorTipoAjustEDeducao.put(tipoAjuste, newMapPorTipoDeducao);
+		}
+	}
+
+	
+	private void populaMapTabela511PorTipoAjuste(Tabela511AjusteApuracaoIcmsSp tbl511, TipoAjusteTabela511 tipoAjuste) {
+		if (isMapNullOrEmpty(mapTabela511PorTipoAjust)) {
+			mapTabela511PorTipoAjust = new HashMap<>();
+		}
+		if (mapTabela511PorTipoAjust.containsKey(tipoAjuste) && tipoAjuste != null) {
+			mapTabela511PorTipoAjust.get(tipoAjuste).add(tbl511);
+		} else {
+			Set<Tabela511AjusteApuracaoIcmsSp> setTabela511 = new HashSet<>();
+			setTabela511.add(tbl511);
+			mapTabela511PorTipoAjust.put(tipoAjuste, setTabela511);
+		}
+	}
+
+	// =====================================================================================================================
+	
+	// NESSA PARTE TENHO QUE PREENCHER o: 			mapTabela511PorTipoAjustEDeducao			
+	
+	// =====================================================================================================================
+	
+	private <K, V> boolean isMapNullOrEmpty(Map<K, V> mapToVerify) {
+		return (isMapNull(mapToVerify) || mapToVerify.isEmpty());
+	}
+
+	private <K, V> boolean isMapNull(Map<K, V> mapToVerify) {
+		return (mapToVerify == null);
+	}
+	
+	private TipoAjusteTabela511 getTipoAjuste(Tabela511AjusteApuracaoIcmsSp tbl511) {
+		Character terceiroCaracter = tbl511.getCodigoAjuste().charAt(2);
+		TipoAjusteTabela511 tipoAjuste = getTipoAjuste(terceiroCaracter);
+		return tipoAjuste;
 	}
 
 
 	/**
-	 * Retorna uma lista com os códigos da Tabela 5.1.1 - "...Ajuste apuracao ICMS SP...", pelo {@link TipoAjusteTabela511}
+	 * Será retornado o de TipoAjuste, de ICMS, conforme o pelo TERCEIRO CARACTERE do COD. AJUSTE;
 	 * 
-	 * @param tipoAjuste
+	 * @param terceiroCaracter
 	 * @return
 	 */
-	public Set<Tabela511AjusteApuracaoIcmsSp> buscarPorTipoAjuste(TipoAjusteTabela511 tipoAjuste) {
-		Map<TipoAjusteTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> mapTabela511PorTipoAjuste = getMapTabela511PorTipoAjuste();
-		Set<Tabela511AjusteApuracaoIcmsSp> listTabela511PorTipoAjust = mapTabela511PorTipoAjuste.get(tipoAjuste);
-		
-		return listTabela511PorTipoAjust;
-	}
-	
-	
-	private Map<TipoAjusteTabela511, Map<TipoDeducaoTabela511, Set<Tabela511AjusteApuracaoIcmsSp>>> getMapTabela511PorTipoAjusteEDeducao(
-			TipoAjusteTabela511 tipoAjuste, TipoDeducaoTabela511 tipoDeducao) {
-		// TODO Auto-generated method stub
-		Map<TipoAjusteTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> mapTabela511PorTipoAjuste = getMapTabela511PorTipoAjuste();
-		return null;
-	}
-
-
-	private Map<TipoAjusteTabela511, Set<Tabela511AjusteApuracaoIcmsSp>> getMapTabela511PorTipoAjuste() {
-		if (mapTabela511PorTipoAjust == null || mapTabela511PorTipoAjust.isEmpty()) {
-			List<Tabela511AjusteApuracaoIcmsSp> listAjustesTabela511 = findAll();
-			
-			listAjustesTabela511.stream().forEachOrdered(tbl511 -> {
-				Character terceiroCaracter = tbl511.getCodigoAjuste().charAt(2);
-				TipoAjusteTabela511 tipoAjuste = getTipoAjuste(terceiroCaracter);
-				populaMapTabela511PorTipoAjuste(tbl511, tipoAjuste);
-			});
-		}
-		return mapTabela511PorTipoAjust;
-	}
-	
-	private void populaMapTabela511PorTipoAjuste(Tabela511AjusteApuracaoIcmsSp tbl511, TipoAjusteTabela511 tipoAjuste) {
-		if (mapTabela511PorTipoAjust.containsKey(tipoAjuste) && tipoAjuste != null) {
-			mapTabela511PorTipoAjust.get(tipoAjuste).add(tbl511);
-		} else {
-			Set<Tabela511AjusteApuracaoIcmsSp> listTabela511 = new HashSet<>();
-			listTabela511.add(tbl511);
-			mapTabela511PorTipoAjust.put(tipoAjuste, listTabela511);
-		}
-	}
-
 	private TipoAjusteTabela511 getTipoAjuste(Character terceiroCaracter) {
 		TipoAjusteTabela511 tipoAjuste = null;
 		for (TipoAjusteTabela511 tpAjuste : getListTiposAjustes()) {
@@ -125,6 +183,19 @@ public class Tabela511AjusteApuracaoIcmsSpService {
 	}
 
 	
+	private TipoDeducaoTabela511 getTipoDeducao(Tabela511AjusteApuracaoIcmsSp tbl511) {
+		Character quartoCaracter = tbl511.getCodigoAjuste().charAt(3);
+		TipoDeducaoTabela511 tipoDeducao = getTipoDeducao(quartoCaracter);
+		return tipoDeducao;
+	}
+	
+	
+	/**
+	 * É retornado o TipoDeducao, conforme o QUARTO CARACTERE do COD. AJUSTE;
+	 * 
+	 * @param quartoCaracter
+	 * @return
+	 */
 	private TipoDeducaoTabela511 getTipoDeducao(Character quartoCaracter) {
 		TipoDeducaoTabela511 tipoDeducao = null;
 		 for (TipoDeducaoTabela511 tpDeducao : getListTiposDeducao()) {
@@ -135,7 +206,7 @@ public class Tabela511AjusteApuracaoIcmsSpService {
 		 return tipoDeducao;
 	}
 	
-
+	
 	private List<TipoAjusteTabela511> getListTiposAjustes() {
 		List<TipoAjusteTabela511> listTipoAjust =  Arrays.asList(TipoAjusteTabela511.values());
 		return listTipoAjust;
