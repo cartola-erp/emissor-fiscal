b9954e06e8ce07f2a17af57f708597fb6848834e
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-09-10 12:48:47 -0300

    [update] atualizando icms_aliq p/ GO (de 12% -> 7%)

--- src/main/resources/db/migration/mysql/cadastro-tributacao-interestadual/V0011__2_VENDA_INTERESTADUAL_JURIDICA_PRODUTO_NACIONAL.sql
+++ src/main/resources/db/migration/mysql/cadastro-tributacao-interestadual/V0011__2_VENDA_INTERESTADUAL_JURIDICA_PRODUTO_NACIONAL.sql
@@ -112,18 +112,18 @@ INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_
  (SELECT 199900, 6102 , 0.0, "CONSUMO",0.07, 0.17, 1.000000, 00, 1.000000, 0.000000, '', "NORMAL", 
  0, 8, 26, ncm_id,2, false FROM ncms WHERE ncm_id NOT IN (SELECT ncm_id FROM trib_esta WHERE oper_id = 2 and esta_orig_id = 26 AND esta_dest_id = 8 AND is_prod_impor = false)); 
 
--- Importados = false (Aliq interestadual = 12.0 PORCENTO
+-- Importados = false (Aliq interestadual = 7.0 PORCENTO
 -- ==================================================== 2 | VENDA INTERESTADUAL (JURIDICA)  SP x GO ============================================================== 
 
 INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, mens, regime_tributario, 
  cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
- (SELECT cest, 6102 , 0.0, finalidade, 0.12 ,0.17, 1.000000, 00, 1.000000, 0.000000, '', regime_tributario, 
+ (SELECT cest, 6102 , 0.0, finalidade, 0.07 ,0.17, 1.000000, 00, 1.000000, 0.000000, '', regime_tributario, 
  cod_anp, 9, 26, ncm_id,2, false FROM trib_esta WHERE oper_id = 1 AND is_prod_impor = false); 
 
 -- 2 | VENDA INTERESTADUAL (JURIDICA) Cadastrando para os outros ncms (que PROVAVELMENTE nao SAO de AUTOPECAS) 
 INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, mens, regime_tributario, 
  cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
- (SELECT 199900, 6102 , 0.0, "CONSUMO",0.12, 0.17, 1.000000, 00, 1.000000, 0.000000, '', "NORMAL", 
+ (SELECT 199900, 6102 , 0.0, "CONSUMO",0.07, 0.17, 1.000000, 00, 1.000000, 0.000000, '', "NORMAL", 
  0, 9, 26, ncm_id,2, false FROM ncms WHERE ncm_id NOT IN (SELECT ncm_id FROM trib_esta WHERE oper_id = 2 and esta_orig_id = 26 AND esta_dest_id = 9 AND is_prod_impor = false)); 
 
 -- Importados = false (Aliq interestadual = 7.000000000000001 PORCENTO
