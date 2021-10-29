51744dcb92dd8db82695a582725d6fa9553311fa
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-10-29 13:37:04 -0300

    add: modelagem reg E200 e E210

--- src/main/java/net/cartola/emissorfiscal/sped/fiscal/blocoE/RegE200.java
+++ src/main/java/net/cartola/emissorfiscal/sped/fiscal/blocoE/RegE200.java
@@ -1,25 +1,33 @@
 package net.cartola.emissorfiscal.sped.fiscal.blocoE;
 
+import java.time.LocalDate;
+
 import coffeepot.bean.wr.annotation.Field;
 import coffeepot.bean.wr.annotation.Record;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * 11/09/2020
  * @author robson.costa
  */
-//@Record(fields = { 
-//		@Field(name = "reg", maxLength = 4),
-//
-//})
+@Record(fields = { 
+		@Field(name = "reg", maxLength = 4),
+		@Field(name = "uf"),
+		@Field(name = "dtIni"),
+		@Field(name = "dtFin"),
+	    // ==================== FILHOS ====================
+		@Field(name = "regE210")
+
+})
+@Setter
+@Getter
 public class RegE200 {
 	
 	private final String reg = "E200";
-	
-	/** 
-	 * COLOCAR os ATRIBUTOS QUE CONSTAM na DOCUMENTAÇÃO
-	 */
-	
-	
+	private String uf;
+    private LocalDate dtIni;
+    private LocalDate dtFin;	
 	// ==================== FILHOS ====================
 	private RegE210 regE210;
 	
--- src/main/java/net/cartola/emissorfiscal/sped/fiscal/blocoE/RegE210.java
+++ src/main/java/net/cartola/emissorfiscal/sped/fiscal/blocoE/RegE210.java
@@ -1,29 +1,73 @@
 package net.cartola.emissorfiscal.sped.fiscal.blocoE;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import coffeepot.bean.wr.annotation.Field;
 import coffeepot.bean.wr.annotation.Record;
+import lombok.Getter;
+import lombok.Setter;
+import net.cartola.emissorfiscal.sped.fiscal.enums.EnumCodificado;
 
 /**
  * 11/09/2020
  * @author robson.costa
  */
-//@Record(fields = { 
-//		@Field(name = "reg", maxLength = 4),
-//
-//})
+@Record(fields = { 
+	@Field(name = "reg", maxLength = 4),
+	@Field(name = "indMovSt"),
+	@Field(name = "vlSldCredAntSt"),
+	@Field(name = "vlDevolSt"),
+	@Field(name = "vlRessarcSt"),
+	@Field(name = "vlOutCredSt"),
+	@Field(name = "vlAjCreditosSt"),
+	@Field(name = "vlRetencaoSt"),
+	@Field(name = "vlOutDebSt"),
+	@Field(name = "vlAjDebitosSt"),
+	@Field(name = "vlSldDevAntSt"),
+    // ==================== FILHOS ====================
+	@Field(name = "regE220"),
+	@Field(name = "regE240"),
+})
+@Getter
+@Setter
 public class RegE210 {
 	
 	private final String reg = "E210";
-	
-	/** 
-	 * COLOCAR os ATRIBUTOS QUE CONSTAM na DOCUMENTAÇÃO
-	 */
-	
-	
+	private IndicadorDeMovimentoSt indMovSt;
+	private BigDecimal vlSldCredAntSt;
+	private BigDecimal vlDevolSt;
+	private BigDecimal vlRessarcSt;
+	private BigDecimal vlOutCredSt;
+	private BigDecimal vlAjCreditosSt;
+	private BigDecimal vlRetencaoSt;
+	private BigDecimal vlOutDebSt;
+	private BigDecimal vlAjDebitosSt;	
+	private BigDecimal vlSldDevAntSt;
 	// ==================== FILHOS ====================
 	private List<RegE220> regE220;
 	private List<RegE240> regE240;
 	
 }
+
+enum IndicadorDeMovimentoSt implements EnumCodificado {
+
+	SEM_OPERACOES_COM_ST("0"), 
+	COM_OPERACOES_ST("1");
+
+	private String indMovSt;
+
+	IndicadorDeMovimentoSt(String indMovSt) {
+		this.indMovSt = indMovSt;
+	}
+
+	@Override
+	public String getCodigo() {
+		return indMovSt;
+	}
+
+}
+
+
+
+ 
