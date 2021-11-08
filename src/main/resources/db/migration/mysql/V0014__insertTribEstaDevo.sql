c4600b1d49cb8c19c1771f11bd4330f10a1bde2b
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2021-11-08 11:01:35 -0300

    [update] parametrizando pis/cofins, para devolução de consumo

--- src/main/resources/db/migration/mysql/V0014__insertTribEstaDevo.sql
+++ src/main/resources/db/migration/mysql/V0014__insertTribEstaDevo.sql
@@ -107,9 +107,10 @@ INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, fin
 -- ====================================== 23 - DEVOLUCAO MATERIAL CONSUMO =======================================================	
 -- ICMS_CST == null -> Deverá ser usado a mesma que vier na NF do fornecedor							
 INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
-					VALUES	(null, 5101, 1556, 5556, "COMERCIALIZACAO", "NORMAL", "", 23);
+					VALUES	(90, 5101, 1556, 5556, "COMERCIALIZACAO", "NORMAL", "", 23);
 INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
-					VALUES	(null, 5102, 1556, 5556, "COMERCIALIZACAO", "NORMAL", "", 23);
+					VALUES	(90, 5102, 1556, 5556, "COMERCIALIZACAO", "NORMAL", "", 23);
+-- as cst acima devem ser "null" ! ? (Ou alguma outra forma que indique que eu tenha que pegar a do fornecedor )
 
 -- Destacamos o ICMS na Devolução (Veio na compra mas não podemos nos creditar)
 -- Verificar se nessas cfop abaixo (5401, 5402 e 5403 temos que mencionar o ICMS na devolução, caso sim acredito que o ICMS CST tenha que ser != 90)
@@ -133,7 +134,29 @@ INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, fin
 
 
 -- ============================== 28 - DEVOLUCAO MERC. DESTINADA A USO E CONSUMO EM OP. C/ST ========================================
+-- ICMS_CST == null -> Deverá ser usado a mesma que vier na NF do fornecedor							
+INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
+					VALUES	(90, 5101, 1556, 5556, "COMERCIALIZACAO", "NORMAL", "", 28);
+INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
+					VALUES	(90, 5102, 1556, 5556, "COMERCIALIZACAO", "NORMAL", "", 28);
+-- as cst acima devem ser "null" ! ? (Ou alguma outra forma que indique que eu tenha que pegar a do fornecedor )
 
-
-
+-- Destacamos o ICMS na Devolução (Veio na compra mas não podemos nos creditar)
+-- Verificar se nessas cfop abaixo (5401, 5402 e 5403 temos que mencionar o ICMS na devolução, caso sim acredito que o ICMS CST tenha que ser != 90)
+INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
+					VALUES	(90, 5401, 1407, 5413, "COMERCIALIZACAO", "NORMAL", "", 28);
+INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
+					VALUES	(90, 5402, 1407, 5413, "COMERCIALIZACAO", "NORMAL", "", 28);
+INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
+					VALUES	(90, 5403, 1407, 5413, "COMERCIALIZACAO", "NORMAL", "", 28);
+					
+INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
+					VALUES	(90, 5405, 1407, 5413, "COMERCIALIZACAO", "NORMAL", "", 28);					
+-- Óleo
+INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
+					VALUES	(90, 5652, 1653, 5662, "COMERCIALIZACAO", "NORMAL", "", 28);
+INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
+					VALUES	(90, 5655, 1653, 5662, "COMERCIALIZACAO", "NORMAL", "", 28);
+INSERT INTO trib_esta_devo (icms_cst, cfop_venda, cfop_entr, cfop_nota_devo, finalidade, regime_tributario , mens, oper_id) 
+					VALUES	(90, 5656, 1653, 5662, "COMERCIALIZACAO", "NORMAL", "", 28);
 -- ==================================================================================================================================		
