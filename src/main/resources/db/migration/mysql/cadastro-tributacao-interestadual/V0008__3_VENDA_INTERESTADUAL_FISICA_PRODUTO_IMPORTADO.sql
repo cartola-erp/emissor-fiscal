fda91f38b0ceaf1d9e3220ec30eabc00e7f2911f
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2022-01-04 12:48:21 -0300

    [update] atualizando o icms_aliq_dest do RS de 17.5% para 17%

--- src/main/resources/db/migration/mysql/cadastro-tributacao-interestadual/V0008__3_VENDA_INTERESTADUAL_FISICA_PRODUTO_IMPORTADO.sql
+++ src/main/resources/db/migration/mysql/cadastro-tributacao-interestadual/V0008__3_VENDA_INTERESTADUAL_FISICA_PRODUTO_IMPORTADO.sql
@@ -313,13 +313,13 @@ INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_
 
 INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, mens, regime_tributario, 
  cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
- (SELECT cest, 6108, 0.0, finalidade, 0.04, 0.175, 1.000000, 00, 1.000000, 0.000000, '', regime_tributario, 
+ (SELECT cest, 6108, 0.0, finalidade, 0.04, 0.17, 1.000000, 00, 1.000000, 0.000000, '', regime_tributario, 
  cod_anp, 23 , 26, ncm_id, 3, true FROM trib_esta WHERE oper_id = 1 and is_prod_impor = true); 
 
 -- Cadastrando para os outros ncms (que PROVAVELMENTE nao SAO de AUTOPECAS) 
  INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, mens, regime_tributario, 
  cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id,  is_prod_impor) 
-	(SELECT 199900,6108,0.0, "CONSUMO", 0.04,0.175, 1.000000, 00, 1.000000, 0.000000, '', "NORMAL", 
+	(SELECT 199900,6108,0.0, "CONSUMO", 0.04,0.17, 1.000000, 00, 1.000000, 0.000000, '', "NORMAL", 
 	0, 23,26, ncm_id,3, true FROM ncms WHERE ncm_id NOT IN (SELECT ncm_id FROM trib_esta WHERE oper_id = 3 and esta_orig_id = 26 and esta_dest_id = 23 and is_prod_impor = true)); 
 
 -- PARA PRODUTOS IMPORTADOS (Aliq 4 PORCENTO) 
