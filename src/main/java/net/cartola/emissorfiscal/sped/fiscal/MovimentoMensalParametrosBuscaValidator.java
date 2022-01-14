58bda5fe42f32fbb2a37c2fd9f5f4fc99ff61512
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2022-01-14 17:51:52 -0300

    update: validacao para informarem o contador | e o inventario caso esteja selecionado a opcao de exportar

--- src/main/java/net/cartola/emissorfiscal/sped/fiscal/MovimentoMensalParametrosBuscaValidator.java
+++ src/main/java/net/cartola/emissorfiscal/sped/fiscal/MovimentoMensalParametrosBuscaValidator.java
@@ -16,19 +16,22 @@ public class MovimentoMensalParametrosBuscaValidator implements Validator {
 
 	@Override
 	public void validate(Object obj, Errors errors) {
-		// TODO Auto-generated method stub
 //		ValidationUtils.rejectIfEmpty(errors, null, null, null);
-//		MovimentoMensalParametrosBusca movMensalParamBusca = (MovimentoMensalParametrosBusca) obj;
 		MovimentoMensalParametrosBusca m = (MovimentoMensalParametrosBusca) obj;
 
 		if ((m.isExportarSpedTodasLojas() && m.isExportarInventario()) && (m.getDataInicioInventario() == null || m.getDataFimInventario() == null) ) {
-//			ValidationUtils.rejectIfEmpty(errors, "dataInicioInventario", "field.require.dataInicioInventario");
-//			ValidationUtils.rejectIfEmpty(errors, "dataFimInventario", "field.require.dataFimInventario");
 			errors.rejectValue("dataInicioInventario", "field.require.dataInicioInventario");
 			errors.rejectValue("dataFimInventario", "field.require.dataFimInventario");
-
-//			errors.rejectValue("data", "data.inicio.fim.inventario.nao.informada");
 		}
+		
+		if (!m.isExportarSpedTodasLojas() && m.isExportarInventario() && m.getInventarioId() == null) {
+			errors.rejectValue("inventarioId", "field.require.inventarioId");
+		}
+		
+		if (m.getContadorId() == null || m.getContadorId().equals(0l)) {
+			errors.rejectValue("contadorId", "field.require.contadorId");
+		}
+		
 	}
 
 }
