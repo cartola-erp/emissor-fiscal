8fec0c7b50b6d4ace61702156cb80f6574455964
Robson Henrique R. Costa <robsonhenrique16@hotmail.com>
2022-02-18 17:31:31 -0300

    update: corrigindo cfop dos ncms validados pela consulcamp

--- src/main/resources/db/migration/mysql/V00002__inserindoNcmsValidadosPelaConsulcamp.sql
+++ src/main/resources/db/migration/mysql/V00002__inserindoNcmsValidadosPelaConsulcamp.sql
@@ -24,12 +24,12 @@ IGNORE 2 LINES
 SET cst = @cst_icms,
 cfop_transferencia = @cfop_transf;	
 
-SELECT c.*, n.ncm_id, n.nume FROM ncms_validados_consulcamp c INNER JOIN ncms n ON (c.ncm = n.nume AND n.exce = 0);
 
 -- colocando os ids dos NCMS para facilitar nos inserts
 UPDATE ncms_validados_consulcamp c INNER JOIN ncms n ON (c.ncm = n.nume AND n.exce = 0) 
 SET c.ncm_id = n.ncm_id;
 
+SELECT c.*, n.ncm_id, n.nume FROM ncms_validados_consulcamp c INNER JOIN ncms n ON (c.ncm = n.nume AND n.exce = 0);
 
 -- Deletando ncms que já estavam previamente cadastrados 
 DELETE FROM trib_esta  where oper_id IN(1,4) and ncm_Id IN (select ncm_id from ncms_validados_consulcamp where ncm_id is not null) ;
@@ -84,6 +84,27 @@ DELETE FROM trib_esta  where oper_id IN(1,4) and ncm_Id IN (select ncm_id from n
 	-- TRANSFERENCIA - cst 60 - importado
 	INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, mens, regime_tributario, 
 		cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
-	(select '199900', c.cfop_transferencia, 0.000000,	'CONSUMO', 0.00000
+	(select '199900', c.cfop_transferencia, 0.000000,	'CONSUMO', 0.000000, 0.000000,	1.000000,	cst,	1.000000,	0.000000,	'',	'NORMAL',	0,	26,	26,	c.ncm_id, 4,	
+		true FROM ncms_validados_consulcamp c where c.ncm_id is not null and c.cst = 60);
 
+	-- TRANSFERENCIA - cst 60 - nacional
+	INSERT INTO `trib_esta` (cest, cfop, fcp_aliq, finalidade, icms_aliq, icms_aliq_dest, icms_base, icms_cst, icms_iva, icms_st_aliq, mens, regime_tributario, 
+		cod_anp, esta_dest_id, esta_orig_id, ncm_id, oper_id, is_prod_impor ) 
+	(select '199900', c.cfop_transferencia, 0.000000,	'CONSUMO', 0.000000, 0.000000,	1.000000,	cst,	1.000000,	0.000000,	'',	'NORMAL',	0,	26,	26,	c.ncm_id, 4,	
+		false FROM ncms_validados_consulcamp c where c.ncm_id is not null and c.cst = 60);
 --  ============================================================================================================================================================
+
+
+-- consultando as tributacoes cadastradas
+
+SELECT * FROM trib_esta  where oper_id IN(1,4) and ncm_Id IN (select ncm_id from ncms_validados_consulcamp where ncm_id is not null) ;
+
+SELECT * FROM trib_esta  where oper_id IN(1,4) and ncm_Id IN (select ncm_id from ncms_validados_consulcamp where ncm_id is not null) 
+and icms_cst = 0;
+
+
+SELECT * FROM trib_esta  where oper_id IN(1,4) and ncm_Id IN (select ncm_id from ncms_validados_consulcamp where ncm_id is not null) 
+and icms_cst = 60;
+
+
+DROP TABLE IF EXISTS ncms_validados_consulcamp;
