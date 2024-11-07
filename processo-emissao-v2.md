ee984963d40c99a59f400720f60fce4bcf9f8246
Murilo Tuvani <murilo.tuvani@gmail.com>
2024-11-07 09:19:28 -0300

    Esboço de processo da emissão na V2

---
+++ processo-emissao-v2.md
@@ -0,0 +1,7 @@
+# Processo de emissão de documentos fiscais na versão 2.0
+
+* Receber o pedido de emissão de documento fiscal e obter os dados relativos ao emitente (Certificado e Ambiente)
+* Preencher os dados fiscais
+* Criar o XML a partir dos dados fiscais preenchidos
+* Enviar o XML para a SEFAZ
+* Imprimir o DANFE (Opcional)
---
+++ src/main/java/net/cartola/emissorfiscal/certificado/Certificado.java
@@ -0,0 +1,58 @@
+package net.cartola.emissorfiscal.certificado;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "cert")
+public class Certificado implements Serializable {
+
+    private Long id;
+    private String senha;
+    private Date vencimento;
+    private String nomeArquivo;
+    private byte[] conteudo;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Date getVencimento() {
+        return vencimento;
+    }
+
+    public void setVencimento(Date vencimento) {
+        this.vencimento = vencimento;
+    }
+
+    public String getNomeArquivo() {
+        return nomeArquivo;
+    }
+
+    public void setNomeArquivo(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    public byte[] getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(byte[] conteudo) {
+        this.conteudo = conteudo;
+    }
+}
