66736b6a692bd1276efa888309d88338b2d7823b
Murilo Tuvani <murilo.tuvani@gmail.com>
2025-01-05 23:48:08 -0300

    Corrigindo o script

--- src/main/resources/db/migration/mysql/V00038__documentoFiscalIndices.sql
+++ src/main/resources/db/migration/mysql/V00038__documentoFiscalIndices.sql
@@ -2,3 +2,8 @@ ALTER TABLE `docu_fisc`
 ADD INDEX `idx_docu_fisc_nume` (`docu` ASC) VISIBLE,
 ADD INDEX `idx_docu_fisc_data_cria` (`criado_por` ASC) INVISIBLE,
 ADD INDEX `idx_docu_fisc_emis` (`emissao` ASC) VISIBLE;
+
+ALTER TABLE `docu_fisc` 
+DROP INDEX `idx_docu_fisc_data_cria` ,
+ADD INDEX `idx_docu_fisc_data_cria` (`cadastro` ASC) INVISIBLE;
+
